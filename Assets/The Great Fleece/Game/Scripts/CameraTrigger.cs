2fce58dddf8f23e49fabfff212bb51d1fd5dccd9
agdodson <andy.dodson@outlook.com>
2021-02-14 17:24:54 +0200

    - Simplified Camera Trigger - Added Start camera postion

--- Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
+++ Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
@@ -9,48 +9,23 @@ public class CameraTrigger : MonoBehaviour
 
 	// debug.log trigger activated
 
-	[SerializeField] private GameObject[] _cameraAngles;
-	[SerializeField] private GameObject[] _cameraTriggers;
-	[SerializeField] private int _cameraIndex;
-
-	private void Start()
-	{
-		string thisTrigger = this.gameObject.name;
-		
-		for (int i = 0; i < _cameraTriggers.Length; i++)
-		{
-			if (thisTrigger == _cameraTriggers[i].name)
-			{
-				_cameraIndex = i;
-				break;
-			}
-		}
-		Debug.Log("thisTrigger , index : " + thisTrigger+","+_cameraIndex);
-	}
-
+	[SerializeField] private GameObject _cameraAngle;
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			Debug.Log("Trigger activated");
-
 			// Assign camera.transform.position == postion of the right camera angle
 			// Assigne the camera.transofmr.rotation == postion of the right camera rotation.
 			Vector3 cameraPos;
-			Quaternion camQuaternion;
 			Vector3 cameraRot;
+			// Enable tracking in inspector which is current camera angle
+			Debug.Log("Current Camera Angle : " + _cameraAngle.name);
+			cameraPos = _cameraAngle.transform.position;
+			cameraRot =  _cameraAngle.transform.eulerAngles;
 
-			cameraPos = _cameraAngles[_cameraIndex].transform.position;
-			cameraRot =  _cameraAngles[_cameraIndex].transform.eulerAngles;
-
-			Debug.Log("camera angle " + _cameraAngles[_cameraIndex].name);
-
-			Debug.Log("Camera : " + _cameraIndex);
-			Debug.Log("CameraProgressionAngles : " + _cameraAngles[_cameraIndex].transform.rotation);
-			Debug.Log("Postions , Roations : " + cameraPos + "," + cameraRot);
-			Camera.main.transform.InverseTransformPoint(cameraPos);
-			Camera.main.transform.rotation = Quaternion.EulerAngles(cameraRot);
+			Camera.main.transform.position = cameraPos;
+			Camera.main.transform.rotation = Quaternion.Euler(cameraRot);
 		}
 	}
 }
