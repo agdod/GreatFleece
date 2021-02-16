755e3b4dda6e6c11cb1a2107582b330fbb7230c8
agdodson <andy.dodson@outlook.com>
2021-02-16 14:25:45 +0200

    - Security Camera completed
    -- Camera freeze
    -- Camera turns red
    -- Actives cutscene ontrigger

--- Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
-    [SerializeField] private CutSceneManager _cutSceneManager;
+	[SerializeField] private CutSceneManager _cutSceneManager;
+
+	private Animator _anim;
+
+	private void Start()
+	{
+		_anim = GetComponentInParent <Animator>();
+	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			_cutSceneManager.EnableCaptured();
+			// Change the material
+			MeshRenderer render = GetComponent<MeshRenderer>();
+			Color color = new Color(0.2f, 0.03f, 0.02f, 0.2f); //(0.6f, 0.1f, 0.1f, 0.3f);
+			render.material.SetColor("_TintColor", color);
+			_anim.enabled = false;
+			StartCoroutine(CutSceneDelay(0.5f));
 		}
+	}
+
+	IEnumerator CutSceneDelay(float delay)
+	{
+		// 0.5 second delay before lunching cutscene
+		yield return new WaitForSeconds(delay);
+		_cutSceneManager.EnableCaptured();
 	}
 }
